56368235d35613819503c02d665313337d99bfb8
Manjot04 <manjotmanjot2004@gmail.com>
2026-01-17 18:53:03 +0530

    exp2update

--- experiment2/src/App.jsx
+++ experiment2/src/App.jsx
@@ -9,11 +9,30 @@ function App() {
       <h2 text className='text-center'>Component for Bootstrap</h2>
       <div className='card p-3 mt-3'>
         <input className='form-control' placeholder='Enter your Name'/>
+        <input className='form-control' placeholder='Enter your Email'/>
         <input className='form-control' placeholder='Enter your Age'/>
         <button className='btn btn-warning'>Submit</button>
       </div>
     </div>
   )
+}
+
+import Button from 'react-bootstrap/Button';
+
+function TypesExample() {
+  return (
+    <>
+      <Button variant="primary">Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="success">Success</Button>
+      <Button variant="warning">Warning</Button>
+      <Button variant="danger">Danger</Button>
+      <Button variant="info">Info</Button>
+      <Button variant="light">Light</Button>
+      <Button variant="dark">Dark</Button>
+      <Button variant="link">Link</Button>
+    </>
+  );
 }
 
 export default App;
